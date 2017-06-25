a09d60289fff21b5a4177baa6d5ddc8436535acb
Matthias Lieberei <matthias.lieberei@gmx.de>
2017-06-25 20:06:37 +0200

    Tried to beautify page with bootstrap.

--- giffer.py
+++ giffer.py
@@ -1,9 +1,14 @@
 import os
 import glob
-from flask import Flask, request, redirect, url_for, send_from_directory
+import threading
+from flask import Flask, request, redirect, url_for, send_from_directory, flash
 from werkzeug.utils import secure_filename
 
 import subprocess
+
+def convert(filename):
+    subprocess.call(["ffmpeg", "-i", "files/"+ filename, "-vf", "scale=320:-1:sws_dither=ed,fps=10,palettegen", "-y", "/tmp/palette.png"])
+    subprocess.call(["ffmpeg", "-i", "files/"+ filename, "-i", "/tmp/palette.png", "-lavfi", "scale=320:-1:sws_dither=ed,fps=10 [x]; [x][1:v] paletteuse", "-y", "files/" + os.path.splitext(filename)[0] + ".gif"])
 
 UPLOAD_FOLDER = 'files/'
 ALLOWED_EXTENSIONS = set(['mp4'])
@@ -37,21 +42,35 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            subprocess.call(["ffmpeg", "-i", "files/"+ filename, "-vf", "scale=320:-1:sws_dither=ed,fps=10,palettegen", "-y", "/tmp/palette.png"])
-            subprocess.call(["ffmpeg", "-i", "files/"+ filename, "-i", "/tmp/palette.png", "-lavfi", "scale=320:-1:sws_dither=ed,fps=10 [x]; [x][1:v] paletteuse", "-y", "files/" + os.path.splitext(filename)[0] + ".gif"])
+            th = threading.Thread(target=lambda : convert(filename))
+            th.start()
             return redirect(url_for('upload_file',
                                     filename=filename))
     files = glob.glob(os.path.join(UPLOAD_FOLDER, "*"))
-    print(files)
-    html_files = "<ul>{}</ul>".format(
-                "\n".join(["<li><a href=/files/{filename}>{filename}</a></li>".format(filename=os.path.basename(file_)) for file_ in files]))
+    html_files = "<ul class=\"list-group\">{}</ul>".format(
+                "\n".join(["<li class=\"list-group-item\"><a href=/files/{filename}>{filename}</a></li>".format(filename=os.path.basename(file_)) for file_ in files]))
     return '''
     <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <h1>Files for Download</h1>
-    ''' + html_files
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <h1>Turn mp4 to gif</h1> 
+    <div class="container">
+    <div class="well">
+    <h2>Upload File</h2>
+        <form method=post enctype=multipart/form-data>
+        <input type="file" name="file">
+        <div class="text-right">
+        <input type=submit value=Upload>
+        </div>
+        </form>
+    </div>
+    </div>
+    <div class="container">
+    <div class="well">
+    <h2>Files for Download</h2>
+    ''' + html_files + '''</div></div>'''
